442e414fda831b9863456ab7a8549fda97c1a8aa
Azor Ahai <azorahaifreelancer@gmail.com>
2018-04-18 18:34:06 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,12 +1,20 @@
 var api = "http://api.giphy.com/v1/gifs/search?";
 var apiKey = "&api_key=dc6zaTOxFJmzC";
-var query = "&q=trump";
-
+var query = "";
+var textfield;
+var sum;
 
 function setup() {
   noCanvas();
+  textfield = createInput();
+  textfield.input(newText);
+  sum=createButton('submit');
   var url = api + apiKey + query;
   loadJSON(url, gotData);
+}
+
+function newText() {
+   query= "&q="+textfield.value();
 }
 
 function gotData(giphy) {
